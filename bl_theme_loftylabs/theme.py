e3a26719f92d4e1a92df1a1619bc81499b100946
Casey Kinsey <casey.kinsey@gmail.com>
2015-11-29 15:57:39 -0600

    Adds theme metadata

---
+++ bl_theme_loftylabs/theme.py
@@ -0,0 +1,7 @@
+__theme_meta__ = {
+    'author': "Casey Kinsey",
+    'title': "Lofty Labs",
+    'url': "https://github.com/loftylabs/bl-theme-loftylabs/",
+    'version': "0.1a",
+    'description': "Theme for Lofty Labs marketing website.",
+}
